0dd81461138cb4711f5c74fe463f7fe86ce63449
chiaranarducci <cnarducci@pioix.edu.ar>
2025-10-07 14:39:13 -0300

    a

---
+++ proyecto-next/front/public/src/app/App.js
@@ -0,0 +1,18 @@
+import {Testimonio} from "./COMPONENTES/Testimonio";
+import React from "react";
+export function App() {
+    return (
+        <div className="App">
+            <h1>blabla</h1>
+            <Testimonio 
+            nombre="chiara narducci"
+            pais="argentina"
+            imagen="chiara"
+            cargo="estudiante"
+            empresa="pio"
+            testimonio="blaaaaaaaaaaaaaaaaaaaaaaa" />
+
+        </div>
+    )
+
+}
